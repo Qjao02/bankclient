e229813df3c71338ac586554fbeb6665803d1d09
Joao Augusto da Cruz <joao_cruz02@hotmail.com>
2018-06-24 23:09:50 -0300

    corrigido erros pt

--- banco-app/src/app/components/sidebar/sidebar-routes.config.ts
+++ banco-app/src/app/components/sidebar/sidebar-routes.config.ts
@@ -6,9 +6,9 @@ export const ROUTES: RouteInfo[] = [
     { path: 'table-list', title: 'Lista Clientes',  icon:'content_paste', class: '' },
     { path: 'novo-funcionario-form', title: 'Novo Funcionario',  icon:'account_box', class: '' },
     { path: 'table-list-funcionario', title: 'Lista Funcionario',  icon:'work', class: '' },
-    { path: 'novo-administrador-form', title:'Novo Administradores', icon:'work_outline', class:''},
-    { path: 'table-list-administrador', title:'Lista Administradores', icon:'work_outline', class:''},
-    { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
+    { path: 'novo-administrador-form', title:'Novo Administrador', icon:'build', class:''},
+    { path: 'table-list-administrador', title:'Lista Administradores', icon:'android', class:''},
+    //{ path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    //{ path: 'maps', title: 'Maps',  icon:'location_on', class: '' },
     //{ path: 'notifications', title: 'Notifications',  icon:'notifications', class: '' },
 ];
